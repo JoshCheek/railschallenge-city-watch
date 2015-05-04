fc4e90616253388aaa889ff4ab11c547cbbe1e9e
Josh Cheek <josh.cheek@gmail.com>
2015-05-03 22:25:35 -0600

    Emergencies can be updated

--- app/controllers/emergencies_controller.rb
+++ app/controllers/emergencies_controller.rb
@@ -25,34 +25,42 @@ class EmergenciesController < ApplicationController
   def show
     emergency = Emergency.find_by code: params[:code]
     return render status: :not_found, json: {} unless emergency
-    render json: {emergency: emergency}
+    render json: { emergency: emergency }
   end
 
   def create
+    emergency_params   = params.require(:emergency).permit(:code, :fire_severity, :police_severity, :medical_severity)
+    unpermitted_params = unpermitted_params(emergency_params)
     emergency = Emergency.new(emergency_params)
 
     if unpermitted_params.any?
       render status: :unprocessable_entity,
              json: { message: "found unpermitted parameter: #{unpermitted_params.join ', '}" }
     elsif emergency.save
-      render status: :created, json: { emergency: emergency.as_json }
+      render status: :created, json: { emergency: emergency }
     else
-      render status: :unprocessable_entity, json: { message: emergency.errors.as_json }
+      render status: :unprocessable_entity, json: { message: emergency.errors }
     end
   end
 
-  # def update
-  #   require "pry"
-  #   binding.pry
-  # end
+  def update
+    emergency_params   = params.require(:emergency).permit(:fire_severity, :police_severity, :medical_severity)
+    unpermitted_params = unpermitted_params(emergency_params)
+    emergency = Emergency.find_by code: params[:code]
+
+    if unpermitted_params.any?
+      render status: :unprocessable_entity,
+             json: { message: "found unpermitted parameter: #{unpermitted_params.join ', '}" }
+    elsif emergency.update_attributes emergency_params
+      render json: { emergency: emergency }
+    else
+      raise 'unhandled'
+    end
+  end
 
   private
 
-  def emergency_params
-    params.require(:emergency).permit(:code, :fire_severity, :police_severity, :medical_severity)
-  end
-
-  def unpermitted_params
+  def unpermitted_params(emergency_params)
     params[:emergency].keys - emergency_params.keys
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   get    '/emergencies'       => 'emergencies#index'
   post   '/emergencies'       => 'emergencies#create'
   get    '/emergencies/:code' => 'emergencies#show'
-  # patch  '/emergencies/:code' => 'emergencies#update'
+  patch  '/emergencies/:code' => 'emergencies#update'
 end
