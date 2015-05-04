1185451b16957bc34b4918c2e89e11347f9b3202
Josh Cheek <josh.cheek@gmail.com>
2015-05-03 23:16:42 -0600

    Extract Responder into app/models b/c test depends on it

--- app/controllers/responders_controller.rb
+++ app/controllers/responders_controller.rb
@@ -1,29 +1,4 @@
 class RespondersController < ApplicationController
-  class Responder < ActiveRecord::Base
-    establish_connection adapter: 'sqlite3', database: ':memory:'
-
-    connection.create_table table_name do |t|
-      t.string  :type
-      t.string  :name
-      t.integer :capacity
-      t.string  :emergency_code
-      t.boolean :on_duty, default: false
-    end
-
-    self.inheritance_column = nil
-    validates :capacity, presence: true, inclusion: { in: 1..5 }
-    validates :type,     presence: true
-    validates :name,     presence: true, uniqueness: true
-
-    def as_json(*)
-      { emergency_code: emergency_code,
-        type:           type,
-        name:           name,
-        capacity:       capacity,
-        on_duty:        on_duty,
-      }
-    end
-  end
 
   def index
     render json: { emergencies: Responder.all }
---
+++ app/models/responder.rb
@@ -0,0 +1,27 @@
+class Responder < ActiveRecord::Base
+  establish_connection adapter: 'sqlite3', database: ':memory:'
+
+  # TODO: migration
+  connection.create_table table_name do |t|
+    t.string  :type
+    t.string  :name
+    t.integer :capacity
+    t.string  :emergency_code
+    t.boolean :on_duty, default: false
+  end
+
+  self.inheritance_column = nil
+  validates :capacity, presence: true, inclusion: { in: 1..5 }
+  validates :type,     presence: true
+  validates :name,     presence: true, uniqueness: true
+
+  # TODO: can I delete this?
+  def as_json(*)
+    { emergency_code: emergency_code,
+      type:           type,
+      name:           name,
+      capacity:       capacity,
+      on_duty:        on_duty,
+    }
+  end
+end
