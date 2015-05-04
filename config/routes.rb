a0aaaf9407c7cc52e25eb71a14959b912b241fae
Josh Cheek <josh.cheek@gmail.com>
2015-05-03 22:41:16 -0600

    Most of the 404s working (/emergencies/new passes b/c hits the show, and errors differ)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  get    '/emergencies'       => 'emergencies#index'
-  post   '/emergencies'       => 'emergencies#create'
-  get    '/emergencies/:code' => 'emergencies#show'
-  patch  '/emergencies/:code' => 'emergencies#update'
+  get   '/emergencies'       => 'emergencies#index'
+  post  '/emergencies'       => 'emergencies#create'
+  get   '/emergencies/:code' => 'emergencies#show'
+  patch '/emergencies/:code' => 'emergencies#update'
+
+  match '*_', via: :all, to: lambda { |env|
+    body = '{"message":"page not found"}'
+    [ 404,
+      { 'Content-Type'   => 'application/json; charset=utf-8',
+        'Content-Length' => body.length,
+      },
+      [body]
+    ]
+  }
 end
